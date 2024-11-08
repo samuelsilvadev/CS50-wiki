06428881588c714db8660345eb782b03d6eb4c00
Samuel Silva <samuelsilvawb@gmail.com>
2024-11-08 23:40:03 +0000

    feat: add 404 for when title is not found

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -10,6 +10,13 @@ def index(request):
 
 
 def get_by_title(request, title):
+    markdown = util.get_entry(title)
+
+    if markdown is None:
+        return render(request, "encyclopedia/404.html", {
+            "title": title
+        })
+
     return render(request, "encyclopedia/entry.html", {
         "entry": util.get_entry(title),
         "title": title
