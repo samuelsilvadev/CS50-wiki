9f4d0dd2fa1c3940037fbbf794d69da1fbf2dd57
Samuel Silva <samuelsilvawb@gmail.com>
2024-11-09 10:55:23 +0000

    feat: add search

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.get_by_title, name="get_by_title")
+    path("search", views.search_by_title, name="search_by_title"),
+    path("wiki/<str:title>", views.get_by_title, name="get_by_title"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from . import util
 
@@ -20,4 +20,31 @@ def get_by_title(request, title):
     return render(request, "encyclopedia/entry.html", {
         "entry": util.get_entry(title),
         "title": title
+    })
+
+
+def search_by_title(request):
+    search_term = request.GET.get('q', '')
+    all_entries = util.list_entries()
+    filtered_entries = []
+    entry_found = None
+
+    for entry in all_entries:
+        if search_term.lower() == entry.lower():
+            entry_found = entry
+            break
+
+        if search_term.lower() in entry.lower():
+            filtered_entries.append(entry)
+
+    if entry_found is not None or len(filtered_entries) == 0:
+        """
+            Using the redirect has two advantages:
+                - changes the URL to where the entry is at,
+                - avoids duplicating the logic to find the entry
+        """
+        return redirect('get_by_title', title=entry_found)
+
+    return render(request, "encyclopedia/index.html", {
+        "entries": filtered_entries
     })
